49d1f74d44e6ae640c75958d1ec3f36c7228f1f3
Stolar <stolar28@gmail.com>
2019-10-28 08:50:09 +0100

    Init console core project

---
+++ VSCodeTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSCodeTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
